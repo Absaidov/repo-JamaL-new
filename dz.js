58c0134b422c9a9ccc3daacbd7d6775ab48afd5f
JamaL <dabsaidov@bk.ru>
2020-09-19 12:57:51 +0300

    //Home work4

--- dz.js
+++ dz.js
@@ -1,93 +1,56 @@
 'use strcit'
-
-
-//ПУНКТ 2
-
-for (let i = 0; i <= 10; i++) {
-    if (i == 0) {
-        console.log(i + ' - Это ноль')
-    } else if (i % 2 == 0) {
-        console.log(i + " - Это четное число")
-    } else
-        console.log(i + ' - Это не четное число')
+//пункт 1
+function getNumber(num) {
+    if (!Number.isInteger(num) || num < 0 || num > 999) {
+        console.log("Значение должно быть от 0 до 999");
+        return {};
+    }
+    return {
+        units: num % 10,
+        tens: Math.floor(num / 10) % 10,
+        hundreds: Math.floor(num / 100),
+    }
 }
 
 
-//ПУНКТ 3
 
-const post = {
-    author: "John", //вывести этот текст
-    postId: 23,
-    comments: [{
-            userId: 10,
-            userName: "Alex",
-            text: "lorem ipsum",
-            rating: {
-                likes: 10,
-                dislikes: 2 //вывести это число
-            }
-        },
-        {
-            userId: 5, //вывести это число
-            userName: "Jane",
-            text: "lorem ipsum 2", //вывести этот текст
-            rating: {
-                likes: 3,
-                dislikes: 1
-            }
-        },
-    ]
+console.log(getNumber(789));
+
+//пункт 1.1
+
+/*Сделайте в стиле es5, а затем в стиле es6 (по аналогии из урока), конструктор Product, который принимает 
+параметры name и price, сохраните их как свойства объекта. Также объекты типа Product должны иметь метод 
+make25PercentDiscount, который будет уменьшать цену в объекте на 25%. */
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    make25PercentDiscount() {
+        this.price = this.price - (this.price * 25 / 100);
+    }
 }
 
-console.log(post.author);
-console.log(post.comments[0].rating.dislikes);
-console.log(post.comments[1].userId);
-console.log(post.comments[1].text);
+const product1 = new Product('Товар', 250);
+product1.make25PercentDiscount();
+
+console.log(product1);
 
 
 
-//ПУНКТ 4
-const products = [{
-        id: 3,
-        price: 200,
-    },
-    {
-        id: 4,
-        price: 900,
-    },
-    {
-        id: 1,
-        price: 1000,
-    },
-];
-products.forEach(element => {
-    element.price = element.price - element.price * 0.15
+//пункт 1.2
 
-});
-console.log(products);
+function Product123(name, price) {
+    this.name = name;
+    this.price = price;
+}
 
-//ПУНКТ 5
+Product123.prototype.make25PercentDiscount = function () {
+    this.price = this.price - (this.price * 25 / 100);
+};
 
-const products = [{
-        id: 3,
-        price: 127,
-        photos: [
-            "1.jpg", "2.jpg",
-        ]
-    },
-    {
-        id: 5,
-        price: 499,
-        photos: []
-    },
-    {
-        id: 10,
-        price: 26,
-        photos: [
-            "3.jpg"
-        ]
-    }, {
-        id: 8,
-        price: 78,
-    },
-];
+const product2 = new Product('Товар 1', 250);
+product1.make25PercentDiscount();
+
+console.log(product1);
